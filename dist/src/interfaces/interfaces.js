afe59f19061814632cf369cb13541f66ac1c1046
andrei128x <andrei128@yahoo.com>
2021-07-19 11:54:33 +0300

    Delete dist/src directory

--- dist/src/interfaces/interfaces.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.IsEnvList = void 0;
-function IsEnvList(item) {
-    return item && typeof item === 'object';
-}
-exports.IsEnvList = IsEnvList;
--- dist/src/routes/routeManager.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-/**
- * @comment empty for now
- */ 
