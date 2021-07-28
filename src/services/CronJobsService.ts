5996a7d4082a343fdb5f29dffa7e4d852e6c55e4
andrei128x <andrei128@yahoo.com>
2021-07-28 23:36:58 +0300

    Major refactoring - transition to full TS OOP

--- src/services/CronJobsService.ts
+++ src/services/CronJobsService.ts
@@ -6,21 +6,13 @@ import * as sensorsInterface from './SensorsConnector';
 import { Request } from 'express';
 import * as http from 'http';
 
-import * as servicesHttpServer from './HttpUtilities';
-import * as servicesMongoData from './DatabaseConnectorService';
+
 import rateLimit from 'express-rate-limit';
 
 export class Jobs
 {
 
-    static httpServer: http.Server | null = null; // Express service available without database connection
-    static serviceMongoActive: Boolean = false; // global state variable that keeps track of Mongo connection
     static VARS: IEnvVariable;
-
-    private constructor()
-    {
-
-    }
 
     static async init(envData: IEnvVariable)
     {
@@ -28,8 +20,6 @@ export class Jobs
         {
             Jobs.VARS = envData;
 
-            Jobs.httpServer = await servicesHttpServer.initExpressApp(); // Express service available without database connection
-            Jobs.serviceMongoActive = await servicesMongoData.databaseConnect();
 
             Jobs.setUpRouteGateOpener();
             Jobs.setUpRouteGateOpenerMOCK();
@@ -53,223 +43,5 @@ export class Jobs
 
     }
 
-    static setUpRouteOutgoingSensorsData()
-    {
-        const apiLimiter = rateLimit({
-            windowMs: 15 * 60 * 1000, // 15 minutes
-            max: 100
-          });
-
-        // TODO : Analyze the possibility to replace the POLLING API with realtime UDP communication
-        servicesHttpServer.router.get('/api', apiLimiter, async (req: Request, res: any, next: any) =>
-        {
-            try
-            {
-                const data = await servicesMongoData.findSomeDataPromise('senzor_pod', 'temperature');
-                // console.log(`[OK][INDEX] Got database response: ${data} items`);
-
-                const tempData = data.map((x: ISensorModel) => ` "${x.property_value}"`);
-                const timestampData = data.map((x: ISensorModel) => `"${(x.timestamp === undefined ? '?' : x.timestamp)}"`);
-
-                res.status(200).send(JSON.parse(
-                    `{"data": [${tempData}],\n\n "times": [${timestampData}]}`
-                ));
-            }
-            catch (err)
-            {
-                console.log(`[ERROR][INDEX] Setting up router reported: ${err}`);
-            }
-        });
-    }
-
-
-    static async setUpRouteIncomingSensorsStream()
-    {
-        // TODO : MOVE to specific module file !!!
-        sensorsInterface
-            .getJsonDataObservable(Jobs.VARS.URL_DEVICE_TEMP_SENSOR) // observable generates PERIODIC data
-            .subscribe(
-                (data: ISensorData[]) =>
-                {
-                    if (Jobs.serviceMongoActive)
-                        servicesMongoData.insertSomeDataPromise(data);
-                    else
-                        console.log('[ERROR][INDEX] Error at insert: database connection is NOT active');
-                },
-                (err: any) =>
-                {
-                    console.log('[ERROR][INDEX] HTTP Observer connection error: ', err);
-                });
-    }
-
-
-    static async setUpRouteGateOpener()
-    {
-        // MOVE TO ROUTES file !!!
-        // forward request to open the gate to the specific IoT device
-        servicesHttpServer.router.get('/api/click', async (req: Request, res: any, next: any) =>
-        {
-            try
-            {
-                // let data = await servicesHttpServer.getJsonPromise(URL_DEVICE_GATE_OPENER_MOCK) // testing one
-                const data = await servicesHttpServer.requestJsonPromise(Jobs.VARS.URL_DEVICE_GATE_OPENER); // real one
-
-                console.log('[OK][INDEX] Servo button responded correctly');
-
-                /* OLD WAY */
-                // res.contentType = 'application/json';
-                // setTimeout(() => { res.status(200).send({ response: '[OK]' }); }, 2000);
-
-                /* NEW WAY */
-                res.status(200).json({ response: '[OK]' });
-
-            } catch (err)
-            {
-                // console.log('[ERROR][INDEX]Servo API did NOT respond correctly');
-                // res.status(400).send({ response: '[FAIL]' });
-                res.status(400).send({ response: `${err}` });
-            }
-        });
-    }
-
-
-    static async setUpRouteGateOpenerMOCK()
-    {
-        // MOVE to ROUTES file !!!
-        // forward request to open the gate to the specific IoT device
-        servicesHttpServer.router.get('/api/click_test', async (req: Request, res: any, next: any) =>
-        {
-            try
-            {
-
-                const wirePusherURL = `https://wirepusher.com/send?id=Wba8mpgaR&title=Gate Event&message=${new Date().toLocaleTimeString()}&type=YourCustomType&message_id=${Date.now()}`;
-                console.log(wirePusherURL);
-
-                const data = await servicesHttpServer.requestJsonPromise(Jobs.VARS.URL_DEVICE_GATE_OPENER_MOCK) // testing one
-                //const data = await servicesHttpServer.getJsonPromise(VARS.URL_DEVICE_GATE_OPENER_MOCK); // real one
-
-                console.log('[OK][INDEX] MOCK Servo button responded correctly');
-                res.contentType = 'application/json';
-                // setTimeout(() => { res.status(200).send({ response: '[OK]' }); }, 2000);
-
-                res.status(200).send({ response: '[OK]' });
-            } catch (err)
-            {
-                // console.log('[ERROR][INDEX]Servo API did NOT respond correctly');
-                // res.status(400).send({ response: '[FAIL]' });
-                res.status(400).send({ response: `${err}` });
-            }
-        });
-    }
-    static async setUpRouteDoorLightSwitch()
-    {
-        const SWITCH_DEFAULT_STATE = 'unknown'
-        let currentState: string = SWITCH_DEFAULT_STATE;
-
-        // TODO : MOVE to ROUTES file !!!
-        // forward request to open the gate to the specific IoT device
-        servicesHttpServer.router.get('/api/door_switch/:switchState', async (req: Request, res: any, next: any) =>
-        {
-            try
-            {
-
-                let requestedState = req.params.switchState;
-
-                if (requestedState === 'state')
-                {
-                    // res.contentType = 'application/json';
-                    let payload: { data: {}; } = {
-                        "data": {}
-                    }
-
-                    const data = await servicesHttpServer.requestJsonPromise(Jobs.VARS.URL_DEVICE_LIGHT_INFO, { data: payload, method: 'POST' });
-                    console.log(`[OK][INDEX] Light switch button responded: ${data}`);
-
-                    currentState = JSON.parse(data).data.switch || SWITCH_DEFAULT_STATE;
-
-                    res.json({ response: '[OK]', state: currentState });
-                }
-                else
-                {
-                    requestedState = requestedState == 'on' ? 'on' : 'off';
-
-                    let payload: { data: { switch: string; }; } = {
-                        "data": {
-                            "switch": requestedState
-                        }
-                    }
-
-                    const data = await servicesHttpServer.requestJsonPromise(Jobs.VARS.URL_DEVICE_LIGHT_SWITCH, { data: payload, method: 'POST' });
-
-                    console.log(`[OK][INDEX] Light switch button responded: ${data}`);
-                    // res.contentType = 'application/json';
-                    // setTimeout(() => { res.status(200).json({ response: '[OK]' }); }, 2000);
-                    currentState = requestedState;
-                    res.status(200).json({ response: '[OK]', state: currentState });
-                }
-            } catch (err)
-            {
-                console.log('[ERROR][INDEX] Light switch API did NOT respond correctly', err);
-                // res.status(400).json({ response: '[FAIL]' });
-                res.status(400).json({ response: '[FAIL]', state: SWITCH_DEFAULT_STATE });
-            }
-        });
-    }
-
-
-    static setUpPingGateHeartbeat()
-    {
-        // MOVE to CronJobs file
-        const cfgs = {
-            timeout: 2,
-            numeric: false,
-            extra: ['-i .5', '-c 3']
-        };
-
-        const localCronJob1 = new CronJob(() =>
-        {
-            ping.promise.probe(Jobs.VARS.URL_HEARTBEAT_GATE_PING_ADDR, cfgs).then((r: any) =>
-            {
-                // console.log(`[OK][INDEX] Ping response: ${r.host} | ${r.numeric_host} | ${r.stddev} | ${r.alive}`);
-                // console.log(`\nRaw response:\n${r.output}`);
-            });
-
-            // TODO: add the ping results to the database
-
-        }, 5000);
-    }
-
-
-    static setUpPingDoorLightHeartbeat()
-    {
-        // MOVE to CronJobs
-        const cfgs = {
-            timeout: 2,
-            numeric: false,
-            extra: ['-i .5', '-c 3']
-        };
-
-        const localCronJob1 = new CronJob(() =>
-        {
-            ping.promise.probe(Jobs.VARS.URL_HEARTBEAT_DOOR_LIGHT_PING_ADDR, cfgs).then((r: any) =>
-            {
-                // console.log(`[OK][INDEX] Ping response: ${r.host} | ${r.numeric_host} | ${r.stddev} | ${r.alive}`);
-                // console.log(`\nRaw response:\n${r.output}`);
-            });
-
-            // TODO: add the ping results to the database
-
-        }, 5000);
-    }
-
-
-    static setUpPeriodicAutoCleanUp()
-    {
-        // MongoDB autoCleanUp of old data
-        const localCronJob2 = new CronJob(() =>
-        {
-            if (Jobs.serviceMongoActive) servicesMongoData.removeSomeData();
-        }, 24 * 3600 * 1000);    // clean-up job runs once every 24 hours
-    }
 
 }
--- src/services/DatabaseConnectorService.ts
+++ src/services/DatabaseConnectorService.ts
@@ -2,124 +2,126 @@
     Purpose: module provides database operation services
     TODO:   select function based on property name
 */
+import { IEnvVariable } from '../interfaces/interfaces';
 
 // connect to MLAB.COM's account
 import { connect as mongooseConnect } from 'mongoose';
 
-// const router = express.Router();
 import { sensorData } from '../models/MongoModel';
 import { throwHere, logOthers } from '../utils/utils';
-import { EnvironmentMapper } from '../utils/environmentMapper';
 
-const mongoServer = EnvironmentMapper.parseEnvironment().URL_MONGO_SERVER;
 
-// connect to the MongoDB database
-export async function databaseConnect()
+export class DatabaseConnectorService
 {
-    let connected;
+    mongoServer: String;
 
-    try
+    constructor(envData: IEnvVariable)
     {
-        if (!mongoServer)
-            throwHere();
+        this.mongoServer = envData?.URL_MONGO_SERVER;
 
-        await mongooseConnect
-            (
-                mongoServer
-                , { useNewUrlParser: true, useUnifiedTopology: true }
-                , err =>
-                {
-                    if (err) 
+        this.databaseConnect();
+    }
+
+    // connect to the MongoDB database
+    databaseConnect()
+    {
+        let connected;
+
+        try
+        {
+            if (this.mongoServer)
+                throwHere();
+
+            await mongooseConnect
+                (
+                    this.mongoServer
+                    , { useNewUrlParser: true, useUnifiedTopology: true }
+                    , err =>
                     {
-                        console.log(`[DB][CONNECT] callback result: ${err}`);
-                        // console.log(`[DB][CONNECT] callback result: ${err}; throwing error so it does not hang`);
-                        // throw (err);
+                        if (err) 
+                        {
+                            console.log(`[DB][CONNECT] callback result: ${err}`);
+                            // console.log(`[DB][CONNECT] callback result: ${err}; throwing error so it does not hang`);
+                            // throw (err);
+                        }
                     }
-                }
-            );
+                );
 
-        console.log(`[OK][DB] Connected to MongoDB database`);
-        connected = true;
+            console.log(`[OK][DB] Connected to MongoDB database`);
+            connected = true;
+        }
+        catch (err)
+        {
+            console.log(`[ERROR][DB] ${err}`);
+            connected = false;
+        }
+
+        return connected;
     }
-    catch (err)
+
+    // insert array of properties into the MongoDB database
+    insertSomeDataPromise(data: any[])
     {
-        console.log(`[ERROR][DB] ${err}`);
-        connected = false;
+        try
+        {
+            sensorData.insertMany
+                (
+                    data,
+                    {},
+                    (error: any, dataAdded: any) =>
+                    {
+                        if (error)
+                        {
+                            console.log('[DB] Error at insert : ', error);
+                        } else
+                        {
+                            // console.log('Date introduse: ', dataAdded);
+                        }
+                    });
+        }
+        catch (err)
+        {
+            console.log('[DB][INSERT] Error at insert : ', err);
+        }
     }
 
-    return connected;
-}
-
-// insert array of properties into the MongoDB database
-export async function insertSomeDataPromise(data: any[])
-{
-    try
+    findSomeDataPromise(deviceName: string, propertyName: string)
     {
-        sensorData.insertMany
-            (
-                data,
+        try
+        {
+            console.log('searching');
+            const data = sensorData.find
+                (
+                    { sensor_name: deviceName, property_name: propertyName },
+                    null,
+                    // { limit: 2880, sort: { _id: -1 }, socketTimeoutMS: 1000 }
+                    { limit: 2880, sort: { _id: -1 } },
+                    // (err: any) => { if (err) { console.log(`[DB][SEARCH] callback result: ${err}; throwing error so it does not hang`); } }
+                );
+            return data;
+        }
+        catch (err)
+        {
+            console.log('[DB] Error connecting to MongoDB database - FIND function');
+            return [];  // returns empty set
+        }
+    }
+
+    removeSomeData(daysAgo?: any)
+    {
+        try
+        {
+            const result = sensorData.deleteMany(
+                { timestamp: { $lt: String(Date.now() - 1000 * 3600 * 24 * 7 * 6) } }, // more than 6 weeks old to milliseconds
                 {},
-                (error: any, dataAdded: any) =>
-                {
-                    if (error)
-                    {
-                        console.log('[DB] Error at insert : ', error);
-                    } else
-                    {
-                        // console.log('Date introduse: ', dataAdded);
-                    }
-                });
-    }
-    catch (err)
-    {
-        console.log('[DB][INSERT] Error at insert : ', err);
-    }
-}
-
-export async function findSomeDataPromise(deviceName: string, propertyName: string)
-{
-    try
-    {
-        console.log('searching');
-        const data = sensorData.find
-            (
-                { sensor_name: deviceName, property_name: propertyName },
-                null,
-                // { limit: 2880, sort: { _id: -1 }, socketTimeoutMS: 1000 }
-                { limit: 2880, sort: { _id: -1 } },
-                // (err: any) => { if (err) { console.log(`[DB][SEARCH] callback result: ${err}; throwing error so it does not hang`); } }
+                (err: any) => { if (true) { console.log(`[DB][CLEAN-UP] callback result: ${err}`); } }
             );
-        return data;
-    }
-    catch (err)
-    {
-        console.log('[DB] Error connecting to MongoDB database - FIND function');
-        return [];  // returns empty set
+
+            console.log(`[OK][DB] Cleaned-up succeeded`);
+        }
+        catch (err)
+        {
+            logOthers('[ERROR][SENSORS] Exception in removeSomeData function');
+        }
     }
 }
-
-export async function removeSomeData(daysAgo?: any)
-{
-    try
-    {
-        const result = sensorData.deleteMany(
-            { timestamp: { $lt: String(Date.now() - 1000 * 3600 * 24 * 7 * 6) } }, // more than 6 weeks old to milliseconds
-            {},
-            (err: any) => { if (true) { console.log(`[DB][CLEAN-UP] callback result: ${err}`); } }
-        );
-
-        console.log(`[OK][DB] Cleaned-up succeeded`);
-    }
-    catch (err)
-    {
-        logOthers('[ERROR][SENSORS] Exception in removeSomeData function');
-    }
-}
-
-// EXPORTS
-module.exports = {
-    databaseConnect,
-    insertSomeDataPromise,
-    findSomeDataPromise,
-    removeSomeData
-};
